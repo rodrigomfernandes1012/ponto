f1b18fc0f259abe8b900c0d06cc42ccf1ed2906c
rodrigomfernandes1012 <166647684+rodrigomfernandes1012@users.noreply.github.com>
2024-09-05 17:51:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import json
-
-from flask import Flask, request, render_template
+from io import BytesIO
+from flask import Flask, request, render_template, send_file, jsonify, jsonify,  render_template,  request, redirect, flash,  url_for
 import pandas as pd
-import os
 from sqlalchemy import create_engine, text
+import mysql.connector
+import os
+import json
 
-app = Flask(__name__)
+def conecta_bd():
+  conexao = mysql.connector.connect(
+  host='dbintellimetrics.c3kc6gou2fhz.us-west-2.rds.amazonaws.com',
+  user='admin',
+  password='IntelliMetr!c$',
+  database='DbIntelliMetrics')
+  return conexao
 
 
 # Configurações do banco de dados
@@ -15,6 +23,12 @@ DB_HOST = 'dbintellimetrics.c3kc6gou2fhz.us-west-2.rds.amazonaws.com'  # ou o IP
 DB_PORT = '3306'  # Porta padrão do MySQL
 DB_NAME = 'DbIntelliMetrics'  # Nome do seu banco de dados
 TABLE_NAME = 'TbDadosPlanilha'  # Nome da tabela
+
+
+
+
+
+app = Flask(__name__)
 
 
 banco_url = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
@@ -50,9 +64,47 @@ def cubagem():
     return ({'message': 'Peso recebido com sucesso', 'peso': dados}), 200
 
 
-@app.route('/')
+@app.route('/upload')
 def upload_form():
     return render_template('upload.html')
+
+@app.route('/get_planilhas', methods=['GET'])
+def get_planilhas():
+    db_connection = conecta_bd()
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT DISTINCT dsNomePlanilha FROM TbDadosPlanilha")
+    results = cursor.fetchall()
+    planilhas = [row[0] for row in results]
+    cursor.close()
+    db_connection.close()
+    print(planilhas)
+    return jsonify(planilhas)
+
+
+def download_planilha():
+    planilha_selecionada = request.json['planilha']
+    print(planilha_selecionada)
+    db_connection = conecta_bd()
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM TbDadosPlanilha WHERE dsNomePlanilha = %s", (planilha_selecionada,))
+    dados = cursor.fetchall()
+    cursor.close()
+    db_connection.close()
+
+    # Criando um DataFrame com os dados
+    df = pd.DataFrame(dados)
+    print(dados)
+
+    # Usando BytesIO para criar um buffer de memória
+    output = BytesIO()
+    df.to_excel(output, index=False, engine='openpyxl', sheet_name='Dados')
+
+    # Rewind the buffer
+    output.seek(0)
+
+    return send_file(output,
+                     attachment_filename=planilha_selecionada,
+                     as_attachment=True)
 
 
 @app.route('/upload', methods=['POST'])
@@ -83,8 +135,8 @@ def upload_file():
 
         # Conectar ao banco de dados e gravar dados
         gravar_dados_no_banco(df)
-
-        return {'data': data_dict}, 200
+        return redirect(url_for('mostrar_dados'))
+        #return {'data': data_dict}, 200
     except Exception as e:
         return str(e), 400
 
@@ -99,7 +151,7 @@ def mostrar_dados():
         engine = create_engine(banco_url)
 
         # Faz uma consulta para selecionar os dados ordenados pela data mais recente
-        query = text(f"SELECT * FROM TbDadosPlanilha ORDER BY cdPlanilha DESC")  # Supondo que haja uma coluna `id` que indica a ordem
+        query = text(f"SELECT distinct dsNF, dsOrdemRec, dsCodigo, dsDescricao, nrQtde, dsSO, nrLinha, nrQtdeCaixas, nrQtdeRecPecas, dsNumeroSerie, nrPeso, dsDimensoes, dsLocalizacao, dsObs, dsSoLinha, dsTipoArmazenagem, dsNomePlanilha FROM DbIntelliMetrics.TbDadosPlanilha order by cdPlanilha desc")  # Supondo que haja uma coluna `id` que indica a ordem
         with engine.connect() as conn:
             result = conn.execute(query)
             dados = result.fetchall()
@@ -119,6 +171,22 @@ def gravar_dados_no_banco(df):
     # Insere os dados na tabela, substituindo os dados existentes ou adicionando
     df.to_sql(TABLE_NAME, con=engine, if_exists='append', index=False)
 
+@app.route('/export', methods=['POST'])
+def export_data():
+    json_data = request.json
+    dados = json_data['dados']  # Os dados recebidos aqui
+    df = pd.DataFrame(dados)
+
+    # Usando BytesIO para criar um buffer de memória
+    output = BytesIO()
+    # Exportando para Excel
+    df.to_excel(output, index=False, engine='openpyxl', sheet_name='Dados')
+
+    # Rewind the buffer
+    output.seek(0)
+
+    # Enviando o arquivo
+    return send_file(output, download_name="planilha", mimetype='xlsx', as_attachment=True)
 
 def main():
     port = int(os.environ.get("PORT", 80))
